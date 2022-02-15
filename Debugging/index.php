f70a6adacf446b7fbd15038fca4fb2196563dd8f
Jawid <jawidforotan100@gmail.com>
2022-02-15 15:18:37 +0100

    The php installation is finished

--- Debugging/index.php
+++ Debugging/index.php
@@ -3,7 +3,12 @@ echo "Exercise starts here:<hr/> <br/>";
 
 
 function myFunction($x){
-$desc = "The  exercise $x starts here";
+$desc = "The  exercise $x starts here <hr/><br/>";
 echo $desc;
 }
 myFunction(1);
+
+myFunction(2);
+$days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+$monday = $days[0];
+echo $monday;
